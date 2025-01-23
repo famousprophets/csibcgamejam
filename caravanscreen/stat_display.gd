7cb143ecb359b66224b6be0db574e8140c50f11f
famousprophets <124466924+famousprophets@users.noreply.github.com>
2025-01-22 20:39:49 -0500

    changes i made on my arch linux :3

--- caravanscreen/stat_display.gd
+++ caravanscreen/stat_display.gd
@@ -14,6 +14,15 @@ func _process(_delta):
 		hordespeed = "Falling Behind..."
 	else:
 		hordespeed = "Same as yours."
-	
+	var red = Color(1.0,0.0,0.0,1.0)
 	%"Stat Display".text = "Uninjured Soldiers:" + str(hp) + "\nInjured Soldiers: " + str(inj) + "\nGold: " + str(gold) + "\nFood Supply: " + str(food) + "\n Morale: " + str(morale) + " / 10\nCaravan Speed: Roughly" + str(cspeed) + " miles per day\nHorde Speed: " + hordespeed
+	if Travelmanager.disttravel - Travelmanager.hordedist <= 25:
+		%hordelabel.text = "The horde is right behind you!!"
+		%hordelabel.label_settings.font_color = red
+	elif Travelmanager.disttravel - Travelmanager.hordedist >= 25 && Travelmanager.disttravel <=50:
+		%hordelabel.text = "The horde is roughly a day away."
+	elif Travelmanager.disttravel - Travelmanager.hordedist >= 51 && Travelmanager.disttravel <=100:
+		%hordelabel.text = "Rest easy. Your scouts report the horde is several days away."
+	elif Travelmanager.disttravel - Travelmanager.hordedist >= 101 && Travelmanager.disttravel <=200:
+		%hordelabel.text = "Your scouts see no traces of the horde. You won't have to worry about them for a while."
 	await get_tree().create_timer(1.0).timeout
